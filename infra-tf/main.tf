cd1674e22b3d927863ede78f0fbbef50e36e3395
Yasin Cetindil <ycetindil@gmail.com>
2023-04-25 19:24:43 -0700

    Add MSI

--- infra-tf/main.tf
+++ infra-tf/main.tf
@@ -4,21 +4,24 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.50.0"
     }
-    github = {
-      source  = "integrations/github"
-      version = "5.20.0"
-    }
   }
   backend "azurerm" {
     resource_group_name  = "ycetindil"
     storage_account_name = "ycetindil"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_msi              = true
+    subscription_id      = "453194c6-9b5a-46f8-bf6e-6b5a4133ee3a"
+    tenant_id            = "1a93b615-8d62-418a-ac28-22501cf1f978"
   }
 }
 
 provider "azurerm" {
   features {}
+
+  use_msi         = true
+  subscription_id = "453194c6-9b5a-46f8-bf6e-6b5a4133ee3a"
+  tenant_id       = "1a93b615-8d62-418a-ac28-22501cf1f978"
 }
 
 ######################
